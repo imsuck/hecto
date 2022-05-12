fc911b580f66d2eb8b1d2a01cc7256fc326dc65f
imsuck <49095435+imsuck@users.noreply.github.com>
2022-05-12 09:33:38 +0700

    Rendering Tabs

--- src/row.rs
+++ src/row.rs
@@ -28,7 +28,11 @@ impl Row {
                             .graphemes(true)
                             .skip(start)
                             .take(end - start) {
-            result.push_str(grapheme);
+            if grapheme == "\t" {
+                result.push(' ');
+            } else {
+                result.push_str(grapheme);
+            }
         }
 
         result
