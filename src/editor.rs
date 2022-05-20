622141c74a229f9c408475d0d28cacba4fadf007
imsuck <49095435+imsuck@users.noreply.github.com>
2022-05-20 07:19:31 +0700

    Incremental search

--- src/editor.rs
+++ src/editor.rs
@@ -119,7 +119,7 @@ impl Editor {
 
     fn save(&mut self) {
         if self.document.file_name.is_none() {
-            let new_name = self.prompt("Save as: ").unwrap_or(None);
+            let new_name = self.prompt("Save as: ", |_, _, _| {}).unwrap_or(None);
 
             if new_name.is_none() {
                 self.status_message = StatusMessage::from("Save aborted.".to_owned());
@@ -154,7 +154,15 @@ impl Editor {
             },
             (KeyModifiers::CONTROL, KeyCode::Char('s')) => self.save(),
             (KeyModifiers::CONTROL, KeyCode::Char('f')) => {
-                if let Some(query) = self.prompt("Search: ").unwrap_or(None) {
+                if let Some(query) = self
+                    .prompt("Search: ", |editor, _, query| {
+                        if let Some(position) = editor.document.find(query) {
+                            editor.cursor_position = position;
+                            editor.scroll();
+                        }
+                    })
+                    .unwrap_or(None)
+                {
                     if let Some(position) = self.document.find(&query) {
                         self.cursor_position = position;
                     } else {
@@ -389,14 +397,18 @@ impl Editor {
         }
     }
 
-    fn prompt(&mut self, prompt: &str) -> Result<Option<String>> {
+    fn prompt<C>(&mut self, prompt: &str, callback: C) -> Result<Option<String>>
+    where
+        C: Fn(&mut Self, KeyCode, &str),
+    {
         let mut result = String::new();
 
         loop {
             self.status_message = StatusMessage::from(format!("{}{}", prompt, result));
             self.refresh_screen()?;
 
-            match Terminal::read_key()?.code {
+            let key = Terminal::read_key()?.code;
+            match key {
                 KeyCode::Backspace => {
                     if !result.is_empty() {
                         result.truncate(result.len().saturating_sub(1));
@@ -414,6 +426,8 @@ impl Editor {
                 },
                 _ => (),
             }
+
+            callback(self, key, &result);
         }
 
         self.status_message = StatusMessage::from(String::new());
